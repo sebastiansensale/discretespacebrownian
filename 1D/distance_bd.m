35f7f7aa96c975ba8d583709c96966e5de63b14e
sebastiansensale <91163535+sebastiansensale@users.noreply.github.com>
2021-10-16 21:52:33 -0400

    Add files via upload

--- 1D/distance_bd.m
+++ 1D/distance_bd.m
@@ -5,16 +5,16 @@
 
 clc;clear;reset(RandStream.getGlobalStream, sum(100*clock));
 for factor=1:1
-    trajectories=100;D=1;delta=1/5;limit=0; 
+    trajectories=500;D=1;delta=1/5;limit=1;separation=1;
     for i=1:trajectories
         j=1;nostop=1;timeA(1)=0;timeB(1)=0;
-        f=initial(delta,limit);
+        f=initial(delta,limit,separation);
         coordA(1,1)=f(1);coordB(1,1)=f(2);
         
         while nostop
             j=j+1;
             [dtA,probA]=timecont(D,delta,coordA(j-1,:));
-            [dtB,probB]=timecont(D,delta,coordB(j-1,:));
+            [dtB,probB]=timecont(D,delta,coordB(j-1,:)-separation);
             
             timeA(j)=timeA(j-1)+dtA;
             timeB(j)=timeB(j-1)+dtB;
@@ -31,11 +31,11 @@ for factor=1:1
                 coordB([1:idmax-2],:)=[];
                 j=size(timeA,2);
             end;            
-            clearvars -except j timeA timeB coordA coordB i trajectories nostop D delta limit endtime  TIM factor
+            clearvars -except j timeA timeB coordA coordB i trajectories nostop D delta limit endtime  TIM factor separation mu
         end;
         
         TIM(i)=endtime;
-        clearvars -except TIM i trajectories factor D delta limit
+        clearvars -except TIM i trajectories factor D delta limit separation mu 
     end;
     Tim=TIM(TIM~=0);
     mu(factor)=mean(nonzeros(Tim));
@@ -48,6 +48,11 @@ for factor=1:1
     clearvars -except mu factor 
 end;
 
-
+    nombre=strcat('table.txt');
+    fid = fopen(nombre, 'w');
+    for i=1:size(mu,2)
+        fprintf(fid, '%f\n', mu(i));
+    end;
+    fclose(fid);
 
 
